9e109c8dd211243238e9bb4f8556fa498c3a53ad
impolitetulip <87053379+impolitetulip@users.noreply.github.com>
2021-07-21 23:25:44 -0500

    Added console app to make github api requests

---
+++ TestReporterBadge/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+
+namespace TestReporterBadge
+{
+    class Program
+    {
+        private static RestClient client = new RestClient();
+        private static string baseAddress = "https://api.github.com/";
+        private static string user;
+        private static string repo;
+
+        static void Main(string[] args)
+        {
+            client.BaseUrl = new Uri(baseAddress);
+            user = "impolitetulip";
+            repo = "test-reporter-badge";
+            GetActionRuns(user, repo);
+        }
+
+        static void GetActionRuns(string user, string repo)
+        {
+            RestRequest request = new RestRequest("repos/" + user + '/' + repo + "/actions/runs");
+            IRestResponse response;
+
+            try
+            {
+                response = client.Get(request);
+
+                if(response.StatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    Console.WriteLine("ERROR - Status Code " + response.StatusCode.ToString());
+                }
+
+                Console.WriteLine(response.Content);
+                JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            }
+            catch(Exception)
+            {
+
+            }
+
+            //return response;
+        }
+    }
+}
